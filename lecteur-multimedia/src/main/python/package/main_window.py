bae7de6c65be68291eabd96dec2c01bf1877dc41
Thibault Houdon <houdon.thibault@gmail.com>
2019-09-25 10:27:37 +0200

    Add icons for file explorer and media player and add open function and shortcut for the media player

--- lecteur-multimedia/src/main/python/package/main_window.py
+++ lecteur-multimedia/src/main/python/package/main_window.py
@@ -8,6 +8,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        self.open_icon = self.style().standardIcon(QtWidgets.QStyle.SP_DriveDVDIcon)
         self.play_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaPlay)
         self.previous_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaSkipBackward)
         self.pause_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaPause)
@@ -27,6 +28,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.file_menu = self.menuBar().addMenu("&File")
 
         # ACTIONS
+        self.act_open = self.file_menu.addAction(self.open_icon, "Open")
+        self.act_open.setShortcut("Ctrl+O")
         self.act_play = self.toolbar.addAction(self.play_icon, "Play")
         self.act_previous = self.toolbar.addAction(self.previous_icon, "Previous")
         self.act_pause = self.toolbar.addAction(self.pause_icon, "Pause")
@@ -35,15 +38,19 @@ class MainWindow(QtWidgets.QMainWindow):
     def add_widgets_to_layouts(self):
         self.addToolBar(self.toolbar)
         self.setCentralWidget(self.video_widget)
-        self.file_menu.addAction(self.play_icon, "Open")
         self.player.setVideoOutput(self.video_widget)
 
     def setup_connections(self):
         self.player.stateChanged.connect(self.update_buttons)
-        self.act_play.triggered.connect(self.player.play)
+        self.act_open.triggered.connect(self.open)
+        self.act_play.triggered.connect(self.play)
         self.act_previous.triggered.connect(partial(self.player.setPosition, 0))
         self.act_pause.triggered.connect(self.player.pause)
         self.act_stop.triggered.connect(self.player.stop)
+
+    def play(self):
+        self.player.play()
+        self.video_widget.resize(QtCore.QSize(1, 1))
 
     def open(self):
         file_dialog = QtWidgets.QFileDialog(self)
