2191d4dea17f406c5db741a4e54241fa47ad8eeb
Thibault Houdon <houdon.thibault@gmail.com>
2019-10-02 15:44:42 +0200

    Remove unused constants

--- PyNotes/src/main/python/package/api/constants.py
+++ PyNotes/src/main/python/package/api/constants.py
@@ -1,6 +1,4 @@
 import os
 from pathlib import Path
 
-CUR_DIR = os.path.realpath(os.path.dirname(__file__))
-PACKAGE_DIR = os.path.dirname(CUR_DIR)
 NOTES_DIR = os.path.join(Path.home(), ".prise-de-note")
